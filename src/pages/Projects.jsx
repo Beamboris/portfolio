b477c126c6e87791402cb948557e953a3af57adf
Beamboris <boris.petrovic@students.beaminstitute.org>
2024-08-02 17:26:27 +0200

    everything

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./project.css";
 import cinemaScope from "/src/assets/Screenshot 2024-07-31 at 00.00.31.png";
-import githubIcon from '/src/assets/icons8-github-40.png';
-import browserIcon from '/src/assets/icons8-browser-100.png'
+import reactIcon from '/src/assets/icons8-react-50.png';
+import tailwindIcon from '/src/assets/icons8-tailwind-css-48.png';
+import githubIcon from '/src/assets/icons8-github-50.png';
+import browserIcon from '/src/assets/icons8-website-48.png';
 
 const Projects = () => {
   return (
@@ -11,7 +13,22 @@ const Projects = () => {
 
       <h5>CinemaScope</h5>
       <div className="project">
+        <div className="img-container">
         <img className="cinemascope" src={cinemaScope} alt="Cinemascope" />
+        <div className="overlay">
+          <p>Technologies used</p>
+          <div className="tech">
+            <div>
+            <img src={reactIcon} alt="react" />
+            <p>React</p>
+            </div>
+            <div>
+            <img src={tailwindIcon} alt="tailwind" />
+            <p>Tailwind</p>
+            </div>
+          </div>
+        </div>
+        </div>
           <div className="case-study">
           <p>
           For my first major project, I drew inspiration from popular streaming platforms like <span className="netflix"><a href="https://www.netflix.com/de-en/" target="_blank">Netflix</a></span> and <span className="disney"><a href="https://www.disneyplus.com/en-de" target="_blank">Disney</a></span> to create a lookalike using ThemovieDB. To keep things user-friendly, I opted to eliminate the login process. Instead, I utilized Local storage to save users favorite movies. Simply click "Sign In," and you're all set to explore!
@@ -19,11 +36,11 @@ const Projects = () => {
           <div className="icons">
             <div className="icon">
           <span><img src={githubIcon} alt="github" /></span>
-          <a href="https://github.com/Beamboris/Cinemascope" target="_blank">Code</a>
+          <a className="project-buttons" href="https://github.com/Beamboris/Cinemascope" target="_blank">Code</a>
           </div>
           <div className="icon">
           <span><img src={browserIcon} alt="website" /></span>
-          <a href="https://cinemascopee.netlify.app/" target="_blank">Live Website</a>
+          <a className="project-buttons" href="https://cinemascopee.netlify.app/" target="_blank">Live Website</a>
           </div>
           </div>
         </div>
